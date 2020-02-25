cb30a6ed7f1d9610a2543d55ff48f7220a094ba4
jamfly <cocoredju222@gmail.com>
2020-02-26 01:39:08 +0800

    Add user model
    Signed-off-by: jamfly <cocoredju222@gmail.com>

---
+++ model/user.go
@@ -0,0 +1,13 @@
+package model
+
+/*
+User contains user information while user register the game
+*/
+type User struct {
+	Name           string `json:"name" form:"name"`
+	Gender         string `json:"gender" form:"gender"`
+	Mail           string `json:"email" form:"mail"`
+	School         string `json:"school" form:"school"`
+	Department     string `json:"department" form:"department"`
+	IsAgreePrivacy bool   `json:"is_agreement_privacy" form:"is_agreement_privacy"`
+}
