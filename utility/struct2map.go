9f5ae4f04a1bd444ad8aea2d434087b09d8aeb1e
jamfly <cocoredju222@gmail.com>
2020-02-26 12:28:02 +0800

    Add struct2map
    Signed-off-by: jamfly <cocoredju222@gmail.com>

---
+++ utility/struct2map.go
@@ -0,0 +1,19 @@
+package utility
+
+import (
+	"reflect"
+)
+
+/*
+Struct2Map transfrom given struct to map format
+*/
+func Struct2Map(obj interface{}) map[string]interface{} {
+	t := reflect.TypeOf(obj)
+	v := reflect.ValueOf(obj)
+
+	var data = make(map[string]interface{})
+	for i := 0; i < t.NumField(); i++ {
+		data[t.Field(i).Name] = v.Field(i).Interface()
+	}
+	return data
+}
