5b2ec7e365640a45b9395b6a9e81d80b6546c8e1
jamfly <cocoredju222@gmail.com>
2020-03-03 17:37:27 +0800

    Fix cors allow path
    Signed-off-by: jamfly <cocoredju222@gmail.com>

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 // NewCORSMiddleware add support of cors
 func NewCORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "localhost")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
